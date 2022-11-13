4bea3f1b506534450c56bed830c62ee3fc59fef9
Maciek K <maciej.kazmierczyk@linkfactory.dk>
2022-11-13 20:11:09 +0100

    :sparkles: Add path backtracking

--- 2. DP - Held Karp/main.py
+++ 2. DP - Held Karp/main.py
@@ -1,4 +1,4 @@
-from src.utils import read_matrix, load_config, print_matrix
+from src.utils import read_matrix, load_config, print_matrix, parse_string_path
 from src.solver import solve_tsp
 from src.logger import Logger
 from src.constants import INPUT_DIR, OUTPUT_DIR
@@ -7,20 +7,23 @@ from src.constants import INPUT_DIR, OUTPUT_DIR
 def main():
     # ladowanie configu
     config = load_config()
+    testing = bool(config['Test'])
+
+    if testing:
+        run_test(config)
+        return
+
     repeats = int(config['Repeats'])
-    stop_1m = bool(config['Stop_After_1m'])
     input_file = config['Input_File']
 
     # wczytywanie macierzy i inicjalizacja loggera wynikow
     matrix = read_matrix(f'{INPUT_DIR}/{input_file}')
-    # print_matrix(matrix)
-    logger = Logger(OUTPUT_DIR, config['Output_File'])
 
     # rozwiazywanie problemu n razy
     output = {}
     # times = []
     for _ in range(repeats):
-        output = solve_tsp(stop_1m, matrix)
+        output = solve_tsp(matrix)
         # times.append(str(output['time']).replace('.', ','))
 
     # dopisanie danych do zapisu
@@ -33,9 +36,31 @@ def main():
     # print('Czasy: ', output['times'])
 
     # zapisywanie wynikow
+    # logger = Logger(OUTPUT_DIR, config['Output_File'])
     # logger.log(output)
     print('\n[OK] Done')
 
+def run_test(config: dict):
+    print('[OK] Testowanie\n')
+    file = config['Test_File']
+    solution = int(config['Test_Solution'])
+    path = parse_string_path(config['Test_Path'])
+
+    matrix = read_matrix(f'{INPUT_DIR}/{file}')
+    result = solve_tsp(matrix)
+
+    solution_match = solution == result['solution']
+    path_match = path == result['path'] or path == tuple(reversed(result['path']))
+
+    print('[✓]' if solution_match else '[✗]', end=' ')
+    print(f'Wynik: {result["solution"]}', end='\n' if solution_match else f' -- Poprawne: {solution}\n')
+    
+    print('[✓]' if path_match else '[✗]', end=' ')
+    print(f'Sciezka: {result["path"]}', end='\n' if path_match else f' -- Poprawne: {path}\n')
+    pass
+
+def run_solve():
+    pass
 
 if __name__ == '__main__':
     # try:
--- 2. DP - Held Karp/src/solver.py
+++ 2. DP - Held Karp/src/solver.py
@@ -1,27 +1,80 @@
 import itertools
 
 
-def solve_tsp(stop_1m: bool, matrix: list):
+def solve_tsp(matrix: list):
     cost = {}
     S = list(range(1, len(matrix)))
+    P = {}
 
     for k in S:
         cost[(k, (k,))] = matrix[0][k]
 
     for k in range(2, len(S) + 1):
-        subsets = list(itertools.combinations(S, k))
-        
+        subsets = itertools.combinations(S, k)
+
         for set in subsets:
             for xi in set:
-                newset = tuple(x for x in set if x != xi)
+                new_set = tuple(x for x in set if x != xi)
 
                 costs = []
-                for xj in newset:
-                    costs.append(cost[(xj, newset)] + matrix[xj][xi])
+                min_cost = float('inf')
+                min_pre_v = None    # wierzcholek poprzedzajacy minimalny koszt
+                min_pre_s = None    # sciezka poprzedzajaca minimalny koszt
 
-                # print(f'cost({xi}, {set}) = min({costs}) = {min(costs)}')
-                cost[(xi, set)] = min(costs)
+                for xj in new_set:
+                    costs.append(cost[(xj, new_set)] + matrix[xj][xi])
+                    if costs[-1] < min_cost:
+                        min_cost = costs[-1]
+                        min_pre_v = xj
+                        min_pre_s = (xj, new_set)
+                        
+                
+                # print(f'cost({xi}, {set}) = min({costs}) = {min_cost} [{min_pre}]')
+                cost[(xi, set)] = min_cost
+                P[(xi, set)] = {'v': min_pre_v, 's': min_pre_s} # zapisanie informacji o poprzedzajacym wierzcholku i sciezce
 
-    max_sets = list(cost)[-len(S):]
-    solution = min([cost[c] + matrix[c[0]][0] for c in max_sets])
-    print(solution)
+    for c in cost:
+        if len(c[1]) == len(S):
+            cost[c] += matrix[c[0]][0]
+        # print(c, ':', cost[c], '::', P.get(c))
+
+    solution, path = backtracking(cost, P, S)
+
+    # print('----------------')
+    # print('Wynik:', solution)
+    # print('Sciezka:', path)
+
+    return {
+        'time': None,
+        'solution': solution,
+        'path': tuple(path)
+    }
+
+def backtracking(cost: dict, P: dict, S: list):
+    cost_keys = list(cost)              # lista kluczy kosztow -- (xi, S)
+    max_len_sets = cost_keys[-len(S):]  # lista najdluzszych zestawow
+
+    solution = float('inf')
+    last_pre_v = None       # ostatni wierzcholek poprzedzajacy
+    last_pre_s = None       # ostatnia sciezka poprzedzajaca
+
+    # znalezienie rozwiazania z najwiekszych zbiorow oraz poprzedzajacego je wierzcholka i sciezki
+    for key in max_len_sets:
+        if cost[key] < solution:
+            solution = cost[key]
+            last_pre_v = key[0]
+            last_pre_s = key
+
+    path = [0, last_pre_v]
+
+    # backtracking po zapisanych poprzedzajacych wierzcholkach i sciezkach
+    cost_keys.reverse()
+    current_set = last_pre_s
+    while True:
+        if len(current_set[1]) == 1:
+            break
+        path.append(P[current_set]['v'])
+        current_set = P[current_set]['s']
+    path.append(0)
+
+    return solution, path
